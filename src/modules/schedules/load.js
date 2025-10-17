9097151917a3e85b5b57e046ad287ef147193739
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-10-17 09:02:19 -0300

    feat: implementa a funcionalidade em buscar todos os atendimentos por dia.

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,14 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import  { hoursLoad } from "../form/hours-load.js"
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
+
+    const dailySchedules = await scheduleFetchByDay({ date });
+    console.log(dailySchedules);
+
     hoursLoad({ date })
 }
