7e85510cf3f19c4d46a927ce42b1e074bc440db7
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-10-21 09:56:58 -0300

    feat: Remove o agendamento.

---
+++ src/services/schedule-cancel.js
@@ -0,0 +1,13 @@
+import { apiConfig } from './api-config.js';
+
+export async function scheduleCancel({ id }) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
+            method: "DELETE",
+        });
+        console.log(id)
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possível cancelar o agendamento.")
+    }
+}
